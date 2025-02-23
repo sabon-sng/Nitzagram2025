ab7a2d595d33253b07e81fe6aae95cd42c0e04b7
User <yehonatan.email@gmail.com>
2025-02-23 16:30:27 +0200

    שלום

--- main.py
+++ main.py
@@ -17,24 +17,33 @@ def main():
     background = pygame.transform.scale(background,(WINDOW_WIDTH, WINDOW_HEIGHT))
 
     # TODO: add a post here
-    img1 = pygame.image.load('Images/ronaldo.jpg')
+    img1 = pygame.image.load('Images/noa_kirel.jpg')
     img1 = pygame.transform.scale(img1,(0.87 * WINDOW_WIDTH, 0.41 * WINDOW_HEIGHT))
 
+    img2 = pygame.image.load('Images/ronaldo.jpg')
+    img2 = pygame.transform.scale(img2,(0.87 * WINDOW_WIDTH, 0.41 * WINDOW_HEIGHT))
 
+    image = [img1,img2]
+    clicks = len(image)
     running = True
     while running:
+        clicks = clicks % len(image)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                clicks += 1
+                clicks = clicks % len(image)
+
         # Grabs events such as key pressed, mouse pressed and so.
         # Going through all the events that happened in the last clock tick
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
 
         # Display the background, presented Image, likes, comments, tags and location(on the Image)
         screen.fill(BLACK)
         screen.blit(background, (0, 0))
 
         # Update display - without input update everything
-        screen.blit(img1, (0.064 * WINDOW_WIDTH, 0.2 * WINDOW_HEIGHT))
+        screen.blit(image[clicks], (0.064 * WINDOW_WIDTH, 0.2 * WINDOW_HEIGHT))
         pygame.display.flip()
 
         # Set the clock tick to be 60 times per second. 60 frames for second.
