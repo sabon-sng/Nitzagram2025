e2edf7ab2a9d0536271df83505531d402e38db4c
User <Noamsonic123@gmail.com>
2025-02-23 17:02:01 +0200

    hello dum dum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from helpers import screen
-from constants import WINDOW_WIDTH, WINDOW_HEIGHT, BLACK
+from constants import*
 
 
 def main():
@@ -15,14 +15,14 @@ def main():
     # Set up background image
     background = pygame.image.load('Images/background.png')
     background = pygame.transform.scale(background,(WINDOW_WIDTH, WINDOW_HEIGHT))
-
+    count = 0
+    font = pygame.font.SysFont("ariel",25)
     # TODO: add a post here
     img1 = pygame.image.load('Images/noa_kirel.jpg')
     img1 = pygame.transform.scale(img1,(0.87 * WINDOW_WIDTH, 0.41 * WINDOW_HEIGHT))
 
     img2 = pygame.image.load('Images/ronaldo.jpg')
     img2 = pygame.transform.scale(img2,(0.87 * WINDOW_WIDTH, 0.41 * WINDOW_HEIGHT))
-
     image = [img1,img2]
     clicks = len(image)
     running = True
@@ -31,10 +31,8 @@ def main():
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                clicks += 1
-                clicks = clicks % len(image)
-
+            if pygame.mouse.get_pos()[0] >= LIKE_BUTTON_X_POS and pygame.mouse.get_pos()[0] <= (LIKE_BUTTON_X_POS + LIKE_BUTTON_WIDTH) and pygame.mouse.get_pos()[1] >= LIKE_BUTTON_Y_POS and pygame.mouse.get_pos()[1] <= (LIKE_BUTTON_Y_POS + LIKE_BUTTON_HEIGHT) and event.type == pygame.MOUSEBUTTONDOWN:
+                count+= 1
         # Grabs events such as key pressed, mouse pressed and so.
         # Going through all the events that happened in the last clock tick
 
@@ -44,6 +42,8 @@ def main():
 
         # Update display - without input update everything
         screen.blit(image[clicks], (0.064 * WINDOW_WIDTH, 0.2 * WINDOW_HEIGHT))
+        like_text = font.render(f"you are liked by {count} people",True,(0,0,0))
+        screen.blit(like_text,(LIKE_TEXT_X_POS,LIKE_TEXT_Y_POS))
         pygame.display.flip()
 
         # Set the clock tick to be 60 times per second. 60 frames for second.
